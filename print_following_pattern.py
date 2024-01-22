5102dce05fa42b153a83fdff5eff3dbbe9d88ef4
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-23 02:13:04 +0800

    Creating a code for loop for rows and column. Additionally, code that will print the pattern

--- print_following_pattern.py
+++ print_following_pattern.py
@@ -1,5 +1,13 @@
 # Print the following pattern
 # Exercise 8
 
-# Creating the pseudocode
+# Create the pseudocode
 
+# Create a code for loop for rows
+for num in range(10):
+    # Create a code for loop for column
+    for i in range(num):
+        # Create a code to print the pattern
+        print (num, end=" ") 
+
+        
